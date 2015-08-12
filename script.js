3c964152658d94376e5ad0dc56df813cf584c6d9
Christophe Sensebé <christophe.sensebe@gmail.com>
2015-08-12 08:12:33 +0000

    adding script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// notification utilisateur si navigateur depasé
+function $buo_f(){ 
+    var e = document.createElement("script"); 
+    e.src = "//browser-update.org/update.min.js"; 
+    document.body.appendChild(e);
+};
+try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
+catch(e){window.attachEvent("onload", $buo_f)}
